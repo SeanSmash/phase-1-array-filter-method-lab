bc71b6d2206a5a69507dd77e9044fcdc24252995
Sean Sarokas <sps4462@gmail.com>
2022-04-22 17:32:03 -0400

    commit tree e28aebdb237c51988eae8c6b07e6e1edb4100265

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+  function findMatching(array, name){
+    return array.filter(function(driver){
+      return driver.toLowerCase() === name.toLowerCase()
+    })
+  }
+
+  function fuzzyMatch(array, name){
+    return array.filter(function(driver){
+      return driver.startsWith(name)
+    })
+  }
+
+  function matchName(array, name){
+      return array.filter(function(driver){
+          return driver["name"] === name
+      })
+  }
